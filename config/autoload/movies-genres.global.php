876d655c99aade725025f1bfbe5be3d3a20c046e
root <root@machine1.puphpet>
2018-08-27 23:04:47 -0500

    adding somethings

---
+++ config/autoload/movies-genres.global.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carlosn
+ * Date: 27/08/18
+ * Time: 10:35 PM
+ */
+return [
+    'service_manager' => [
+        'services' => [
+            'movies-genres' => [
+                'comedia' => [
+                    [
+                        'nombre' => "Ted",
+                        'url' => "https://www.cinerama.es/peliculas/comedia/ted",
+                        'imagen' => "https://www.cinerama.es/wp-content/uploads/09/2012/cine-015858-120x171.jpg",
+                    ],
+                    [
+                        'nombre' => "Baywatch",
+                        'url' => "https://www.cinerama.es/peliculas/comedia/baywatch-los-vigilantes-de-la-playa",
+                        'imagen' => "https://www.cinerama.es/wp-content/uploads/2017/06/cine-037100-120x171.jpg",
+                    ],
+                    [
+                        'nombre' => "El gurú de las bodas",
+                        'url' => "https://www.cinerama.es/peliculas/comedia/el-guru-de-las-bodas",
+                        'imagen' => "https://www.cinerama.es/wp-content/uploads/2015/05/cine-026405-120x171.jpg",
+                    ]
+                ],
+                'accion' => [
+                    [
+                        'nombre' => "Jurassic World: El reino caído",
+                        'url' => "https://www.cinerama.es/peliculas/accion/jurassic-world-el-reino-caido",
+                        'imagen' => "https://www.cinerama.es/wp-content/uploads/2018/06/cine-043220-120x171.jpg",
+                    ],
+                    [
+                        'nombre' => "Everest",
+                        'url' => "https://www.cinerama.es/peliculas/accion/everest",
+                        'imagen' => "https://www.cinerama.es/wp-content/uploads/2015/09/cine-028424-120x171.jpg",
+                    ],
+                    [
+                        'nombre' => "Focus",
+                        'url' => "https://www.cinerama.es/peliculas/accion/focus",
+                        'imagen' => "https://www.cinerama.es/wp-content/uploads/2015/03/cine-026850-120x171.jpg",
+                    ],
+                ],
+                'cienciaFiccion' => [
+                    [
+                        'nombre' => "Avengers Infinity War",
+                        'url' => "https://www.cinerama.es/peliculas/accion/vengadores-infinity-war",
+                        'imagen' => "https://www.cinerama.es/wp-content/uploads/2018/04/cine-042612-120x171.jpg",
+                    ],
+                    [
+                        'nombre' => "El marciano",
+                        'url' => "https://www.cinerama.es/peliculas/ciencia-ficcion/marte-the-martian",
+                        'imagen' => "https://www.cinerama.es/wp-content/uploads/2015/10/cine-028688-120x171.jpg",
+                    ],
+                    [
+                        'nombre' => "Ready Player One",
+                        'url' => "https://www.cinerama.es/peliculas/ciencia-ficcion/ready-player-one",
+                        'imagen' => "https://www.cinerama.es/wp-content/uploads/2018/03/cine-041938-120x171.jpg",
+                    ],
+                ],
+                'terror' => [
+                    [
+                        'nombre' => "Eso",
+                        'url' => "https://www.cinerama.es/peliculas/terror/it-2",
+                        'imagen' => "https://www.cinerama.es/wp-content/uploads/2017/09/cine-036835-120x171.jpg",
+                    ],
+                    [
+                        'nombre' => "Poltergeist",
+                        'url' => "https://www.cinerama.es/peliculas/terror/poltergeist",
+                        'imagen' => "https://www.cinerama.es/wp-content/uploads/2015/05/cine-026774-120x171.jpg",
+                    ],
+                    [
+                        'nombre' => "28 días después",
+                        'url' => "https://www.cinerama.es/peliculas/terror/28-dias-despues",
+                        'imagen' => "https://www.cinerama.es/wp-content/uploads/2003/07/cine-003906-120x171.jpg",
+                    ],
+                ],
+            ]
+        ]
+    ]
+];
